d1348ff32408c4610aab950e6d73979f1b00cfed
assaf <assaf@b757b4cc-fe04-0410-8fec-c912f6db8c99>
2005-11-28 23:22:11 +0000

    git-svn-id: https://labnotes.org/svn/public@55 b757b4cc-fe04-0410-8fec-c912f6db8c99

--- lws/json.rb
+++ lws/json.rb
@@ -3,7 +3,26 @@ require 'strscan'
 
 module JSON
 
-    class Writer
+    def self.dump object, output = nil, indent = 0
+        ser = Serializer::new output, indent
+        ser.write object
+        if output
+            ser.flush
+            output
+        else
+            ser.to_str
+        end
+    end
+
+
+    def self.load input
+        #input = StringIO.new(input) unless input.is_a?(IO)
+        parser = Parser::new
+        parser.read input
+    end
+
+
+    class Serializer
 
         # Regular expression for catching all the characters that must be encoded.
         ENCODE_REGEXP = /[^[:print:]]|["\\]/
@@ -30,36 +49,43 @@ module JSON
             end
         end
 
-        def self.indented &block
-            self.new indent = 4, &block
-        end
+        DEFAULT_INDENT = 4
 
-        def initialize indent = 0, &block
+        def initialize output = nil, indent = 0, &block
             @separator = false
-            @io = StringIO.new
-            @indent_at = @indent_by = indent > 0 ? indent : 0
+            @io = output || StringIO.new
+            @closed = false
+            if indent > 0
+                @indent, @indent_by = 1, " " * indent
+            else
+                @indent, @indent_by = 0, nil
+            end
             @io << '{'
             instance_eval &block if block
         end
 
+
+        def self.indented output = nil, &block
+            self.new output, indent = DEFAULT_INDENT, &block
+        end
+
+
+        def flush
+            close
+            @io.flush
+        end
+
+
         def to_str
-            # Make sure to close object exactly once for each call to this method.
-            unless @io.closed_write?
-                # Closing indentation, since this may be nested in another object.
-                if @indent_by > 0
-                    @indent_at -= @indent_by
-                    @io << "\n"
-                    @indent_at.times { @io << ' ' }
-                end
-                @io << '}'
-                @io.close_write
-            end
+            close
             @io.string
         end
 
-        alias :to_s :to_str
+        alias :to_json :to_str
+
 
         def write *args, &block
+            raise RuntimeError, "Serialized closed for writing" if @closed
             raise ArgumentError, "wrong number of arguments (0 for 1)" unless args.length > 0
             case args[0]
             when Hash
@@ -67,9 +93,9 @@ module JSON
                 name, value = nil, nil
                 args[0].each_pair do |name, value|
                     @io << ',' if @separator
-                    if @indent_by > 0
+                    if @indent_by
                         @io << "\n"
-                        @indent_at.times { @io << ' ' }
+                        @indent.times { @io << @indent_by }
                     end
                     name = name.to_s unless name.instance_of?(String)
                     @io << '"' << name.gsub(ENCODE_REGEXP, &ENCODE_PROC) << '": '
@@ -78,21 +104,21 @@ module JSON
             when String, Symbol, Numeric
                 raise ArgumentError, "expected second argument with value, or a block" unless args.length == 2 || block
                 @io << ',' if @separator
-                if @indent_by > 0
+                if @indent_by
                     @io << "\n"
-                    @indent_at.times { @io << ' ' }
+                    @indent.times { @io << @indent_by }
                 end
                 name = name.instance_of?(String) ? args[0] : args[0].to_s
                 @io << '"' << name.gsub(ENCODE_REGEXP, &ENCODE_PROC) << '": '
                 if block
                     separator, @separator = @separator, false
                     @io << '{'
-                    if @indent_by > 0
-                        @indent_at += @indent_by
+                    if @indent_by
+                        @indent += 1
                         instance_eval &block
-                        @indent_at -= @indent_by
+                        @indent -= 1
                         @io << "\n"
-                        @indent_at.times { @io << ' ' }
+                        @indent.times { @io << @indent_by }
                     else
                         instance_eval &block
                     end
@@ -108,26 +134,16 @@ module JSON
             self
         end
 
-        def object &block
-            writer = Writer.new @indent_by
-            writer.indent_at = @indent_at + @indent_by
-            writer.do &block
-            writer
-        end
 
-        def self.write *args, &block
-            self.new.write(*args, &block).to_s
+        def object &block
+            raise RuntimeError, "Serialized closed for writing" if @closed
+            ser = Serializer.new nil, @indent_by
+            ser.instance_variable_set :@indent,  @indent + 1
+            ser.instance_eval &block
+            ser
         end
 
     private
-
-        def indent_at= count
-            @indent_at = count
-        end
-
-        def do &block
-            instance_eval &block
-        end
 
         def write_value value
             case value
@@ -159,18 +175,18 @@ module JSON
                 @io << '{'
                 separator, @separator = @separator, false
                 name = nil
-                if @indent_by > 0
-                    @indent_at += @indent_by
+                if @indent_by
+                    @indent += 1
                     value.each_pair { |name, value| write name, value }
-                    @indent_at -= @indent_by
+                    @indent -= 1
                     @io << "\n"
-                    @indent_at.times { @io << ' ' }
+                    @indent.times { @io << @indent_by }
                 else
                     value.each_pair { |name, value| write name, value }
                 end
                 @io << '}'
                 @separator = true
-            when Writer
+            when Serializer
                 @io << value.to_str
             else
                 if value.respond_to?(:to_json)
@@ -181,25 +197,61 @@ module JSON
             end
         end
 
+        def close
+            begin
+                # Make sure to close object exactly once for each call to this method.
+                unless @closed
+                    # Closing indentation, since this may be nested in another object.
+                    if @indent_by
+                        @indent -= 1
+                        @io << "\n"
+                        @indent.times { @io << @indent_by }
+                    end
+                    @io << '}'
+                end
+            ensure
+                @closed = true
+            end
+        end
+
     end
 
 
-    class Reader
+    class Parser
+
+        # Regular expression for catching all the characters that must be decoded.
+        DECODE_REGEXP = /\\[nrftb\\\/"]|\\\d{4}/
+
+        # Proc for decoding escaped characters .
+        DECODE_PROC = Proc.new do |match|
+            case match[1]
+            when ?n
+                "\n"
+            when ?r
+                "\r"
+            when ?t
+                "\t"
+            when ?b
+                "\b"
+            when ?f
+                "\f"
+            when ?\\, 34 # TODO: slash
+                match[1]
+            else
+                # TODO: convert four hexadigits to code
+            end
+        end
 
         def initialize
         end
 
-        def read string
-            scanner = StringScanner.new string
+        def read input
+            scanner = StringScanner.new input
             scanner.scan(/\s*\{\s*/) or fail "Expected opening curly bracket ({ at start of object)"
             object = scan_object scanner
             scanner.skip(/\s*/)
             scanner.eos? or fail "Found unexpected data beyond end of JSON object"
             object
-        end
-
-        def self.read string
-            Reader.new.read string
         end
 
     private
@@ -211,7 +263,7 @@ module JSON
                 scanner.scan(/\s*\:\s*/) or fail "Missing name/value separator (:)"
                 object[name] = scan_value scanner
             end while scanner.scan(/\s*,\s*/)
-            scanner.scan(/s*\}\s*/) or fail "Expected closing curly brackets at end of object (})"
+            scanner.scan(/\s*\}\s*/) or fail "Expected closing curly brackets at end of object (})"
             object
         end
 
@@ -224,7 +276,7 @@ module JSON
                 scanned << scanner.scan(/[^\"]*/)
             end
             scanner.skip(/\"/) or fail "Expected closing quote (\" at end of name/value)"
-            scanned
+            scanned.gsub DECODE_REGEXP, &DECODE_PROC
         end
 
         def scan_value scanner
@@ -262,8 +314,9 @@ end
 
 
 
-def test_1
-    puts JSON::Writer.indented {
+def test_write_methods
+    puts "test_write_methods"
+    puts JSON::Serializer.indented {
         write "glossary", object {
             write "title", "example glossary"
             write "GlossDiv", object {
@@ -281,12 +334,13 @@ def test_1
                 ]
             }
         }
-    }
+    }.to_str
 end
 
 
-def test_2
-    json = JSON::Writer.indented
+def test_hash_write
+    puts "test_hash_write"
+    json = JSON::Serializer.indented
     json.write "glossary"=>{
         "title"=>"example glossary",
         "GlossDiv"=>{
@@ -306,19 +360,22 @@ def test_2
 end
 
 
-def test_3
-    puts JSON::Writer.new {
+
+def test_base_types
+    puts "test_base_types"
+    puts JSON::Serializer.indented {
         write "false", false
         write "true", true
         write "null", nil
         write "float", 12.34
         write "integer", 56
         write "array", ["foo", :bar, 56, Time.new, nil]
-    }
+    }.to_str
 end
 
 
-def write_test
+
+def bechmark
 
     require 'benchmark'
     require 'yaml'
@@ -342,67 +399,75 @@ def write_test
     }
     count = 1000
 
-    puts "JSon (indented) size: #{JSON::Writer.indented.write(object).to_s.length}"
+    puts "Serialized text size"
+    json = JSON::dump(object)
+    puts "JSON: #{json.length}"
+    json_in = JSON::dump(object, nil, 4)
+    puts "JSON (indent): #{json_in.length}"
+    yaml = YAML::dump(object)
+    puts "YAML: #{yaml.length}"
+    marshal = Marshal::dump(object)
+    puts "Marshal: #{marshal.length}"
 
-    puts "JSon size: #{JSON::Writer::write(object).to_s.length}"
-    puts Benchmark.measure {
-        count.times do
-            JSON::Writer::write(object).to_s
+    test = Proc.new do |dump, load|
+        Benchmark.bmbm(20) do |bm|
+            bm.report("JSON:") do
+                count.times { JSON::dump(object) } if dump
+                count.times { JSON::load(json) } if load
+            end
+            bm.report("JSON (indent):") do
+                count.times { JSON::dump(object, nil, 4) } if dump
+                count.times { JSON::load(json_in) } if load
+            end
+            bm.report("YAML:") do
+                count.times { YAML::dump(object) } if dump
+                count.times { YAML::load(yaml) } if load
+            end
+            bm.report("Marshal:") do
+                count.times { Marshal::dump(object) } if dump
+                count.times { Marshal::load(marshal) } if load
+            end
         end
-    }
-
-    puts "YAML size: #{YAML::dump(object).length}"
-    puts Benchmark.measure {
-        count.times do
-            YAML::dump(object)
-        end
-    }
-
-    puts "Marshal size: #{Marshal.dump(object).length}"
-    puts Benchmark.measure {
-        count.times do
-            Marshal::dump(object)
-        end
-    }
+    end
+    puts
+    puts "Serialization (dump):"
+    test.call true, false
+    puts
+    puts "Parsing (load):"
+    test.call false, true
+    puts
+    puts "Round-trip (dump, load):"
+    test.call true, true
 
 end
 
 
-object = {
-    "glossary"=>{
-        "title"=>"example glossary",
-        "GlossDiv"=>{
-            "title"=>"S",
-            "GlossList"=>[{
-                "ID"=>"SGML",
-                "SortAs"=>"SGML",
-                "GlossTerm"=>"Standard Generalized Markup Language",
-                "Acronym"=>"SGML",
-                "Abbrev"=>"ISO 8879:1986",
-                "GlossDef"=>"A meta-markup language, used to create markup languages such as DocBook.",
-                "GlossSeeAlso"=>["GML", "XML", "markup"]
-            }]
+def test_roundtrip
+    object = {
+        "glossary"=>{
+            "title"=>"example \n glossary",
+            "GlossDiv"=>{
+                "title"=>"S",
+                "GlossList"=>[{
+                    "ID"=>"SGML",
+                    "SortAs"=>"SGML",
+                    "GlossTerm"=>"Standard Generalized Markup Language",
+                    "Acronym"=>"SGML",
+                    "Abbrev"=>"ISO 8879:1986",
+                    "GlossDef"=>"A meta-markup language, used to create markup languages such as DocBook.",
+                    "GlossSeeAlso"=>["GML", "XML", "markup"]
+                }]
+            }
         }
     }
-}
-string = JSON::Writer.write(object).to_s
-reader = JSON::Reader.new
-object2 = reader.read string
-puts object2 == object
-puts JSON::Writer.indented.write(object2)
+    string = JSON::dump(object).to_s
+    object2 = JSON::load string
+    puts "Round trip completed: #{object2 == object}"
+    puts JSON::dump(object2, nil, 4)
+end
 
-
-require 'benchmark'
-count = 1000
-puts Benchmark.measure {
-    count.times do
-        string = JSON::Writer::write(object).to_s
-        object = JSON::Reader::read(string)
-    end
-}
-puts Benchmark.measure {
-    count.times do
-        string = Marshal::dump(object)
-        object = Marshal::load(string)
-    end
-}
+#test_write_methods
+#test_hash_write
+#test_base_types
+test_roundtrip
+bechmark
