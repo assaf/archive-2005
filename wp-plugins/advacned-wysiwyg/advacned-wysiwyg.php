c4bd5fb9d2862484d6cf7e20d1773956d9d56cc2
assaf <assaf@b757b4cc-fe04-0410-8fec-c912f6db8c99>
2005-12-26 07:16:47 +0000

    First checkin
    git-svn-id: https://labnotes.org/svn/public@71 b757b4cc-fe04-0410-8fec-c912f6db8c99

---
+++ wp-plugins/advacned-wysiwyg/advacned-wysiwyg.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Plugin Name: Advanced WYSIWYG Editor
+Plugin URI: http://www.labnotes.org/
+Description: Adds more styling options to the WYSIWYG post editor.
+Version: 0.1
+Author: Assaf Arkin
+Author URI: http://labnotes.org/
+License: Creative Commons Attribution-ShareAlike
+Tags: wordpress tinymce
+*/
+
+if (isset($wp_version)) {
+    add_filter("mce_plugins", "extended_editor_mce_plugins", 0);
+    add_filter("mce_buttons", "extended_editor_mce_buttons", 0);
+}
+
+
+function extended_editor_mce_plugins($plugins) {
+    array_push($plugins, "table", "fullscreen");
+    return $plugins;
+}
+
+
+function extended_editor_mce_buttons($buttons) {
+return array(
+        "formatselect", "bold", "italic", "underline", "strikethrough", "separator",
+        "bullist", "numlist", "indent", "outdent", "separator",
+        "justifyleft", "justifycenter", "justifyright", "justifyfull", "separator",
+        "link", "unlink", "anchor", "image", "hr", "separator",
+        "cut", "copy", "paste", "undo", "redo", "separator",
+        "table", "sub", "sup", "forecolor", "backcolor", "charmap", "separator",
+        "code", "fullscreen", "wordpress", "wphelp");
+}
+
+?>
