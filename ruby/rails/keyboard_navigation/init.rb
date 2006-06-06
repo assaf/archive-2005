78886291f4bc79736a7054e922089642664dbfe3
assaf <assaf@b757b4cc-fe04-0410-8fec-c912f6db8c99>
2006-06-06 03:14:18 +0000

    git-svn-id: https://labnotes.org/svn/public@102 b757b4cc-fe04-0410-8fec-c912f6db8c99

--- ruby/rails/keyboard_navigation/init.rb
+++
@@ -1,3 +0,0 @@
-ActionView::Helpers::AssetTagHelper.send :register_javascript_include_default, "keyboard"
-ActionView::Base.send :include, KeyboardHelper
-ActionController::Base.send :include, KeyboardHelper
