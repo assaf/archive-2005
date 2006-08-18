442e477a1bcf20f55a34f5eb73e1ae26e4383eef
assaf <assaf@b757b4cc-fe04-0410-8fec-c912f6db8c99>
2006-08-18 07:28:28 +0000

    Fixed: requires in init.rb as reported by Benjamin Birnbaum.
    git-svn-id: https://labnotes.org/svn/public@218 b757b4cc-fe04-0410-8fec-c912f6db8c99

--- ruby/rails_plugins/assert_select/init.rb
+++ ruby/rails_plugins/assert_select/init.rb
@@ -1,3 +1,3 @@
-require File.join(File.dirname(__FILE__), "lib", "assert_select")
-require File.join(File.dirname(__FILE__), "lib", "html_selector")
+require "assert_select"
+require "html_selector"
 Test::Unit::Assertions.send :include, Test::Unit::AssertSelect
