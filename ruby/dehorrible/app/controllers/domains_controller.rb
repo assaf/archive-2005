9749f47c3555814cb8ade0fca73f607dfd6ad766
assaf <assaf@b757b4cc-fe04-0410-8fec-c912f6db8c99>
2007-12-17 09:17:18 +0000

    Added show for domain
    git-svn-id: https://labnotes.org/svn/public@263 b757b4cc-fe04-0410-8fec-c912f6db8c99

--- ruby/dehorrible/app/controllers/domains_controller.rb
+++ ruby/dehorrible/app/controllers/domains_controller.rb
@@ -34,6 +34,33 @@ class DomainsController < ApplicationController
     render :status=>:created, :location=>domain_url(name), :nothing=>true
   end
 
+  # GET to /domains/:domain_id?query=&limit=&token=
+  #
+  # Query parameters:
+  # * query - Query expression
+  # * limit - Maximum number of items to return
+  # * token - Token returned from previous query
+  #
+  # JSON:  Returns [ url*, { token: ... }? ]
+  # XML:   Returns <items><item>url</item><token>token</token>?</items>
+  def show
+    query = { 'query'=>'QueryExpression', 'limit'=>'MaxNumberOfItems', 'token'=>'NextToken' }.
+      inject({}) { |hash, (from, to)| params[from] ? hash.update(to=>params[from]) : hash }
+    response = getsify(query, 'Action'=>'Query')
+    items = response.get_elements('/QueryResponse/QueryResult/ItemName').map(&:text).
+      map { |item| domain_item_url(params['id'], item) }
+    next_token = response.get_text('/QueryResponse/QueryResult/NextToken')
+    respond_to do |format|
+      format.json do
+        items << { :token=>next_token.to_s } if next_token
+        render :json=>items
+      end
+      format.xml  do
+        render :xml=>items.to_xml(:root=>'items') { |xml| xml.tag! 'token', next_token.to_s if next_token }
+      end
+    end
+  end
+
   # DELETE to /domains/:domain_id
   def destroy
     getsify 'Action'=>'DeleteDomain'
